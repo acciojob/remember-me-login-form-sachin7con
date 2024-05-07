8d4f3ea1cfca630ca0b9903b5051336f14b3dfcd
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-07 10:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+//SGNyour JS code here. If required.
+const btn = document.getElementById("submit");
+
+btn.addEventListener('click', (e) =>{
+	e.preventDefault(); 
+	const user = document.getElementById("username").value;
+	const pass = document.getElementById("password").value;
+	const check = document.getElementById("checkbox").checked;
+	const output = document.getElementById("output");
+	
+	if(check){
+		localStorage.setItem( 'username', user);
+		localStorage.setItem( 'password', pass);
+	}else{
+		localStorage.removeItem('username');
+		localStorage.removeItem('password');
+		
+	}
+	alert('Logged in as ' + user);
+	
+});
+
+if(localStorage.getItem('username') && localStorage.getItem('password')){
+	document.getElementById('existing').style.display = 'block';
+
+	document.getElementById('existing').addEventListener('click', () => {
+		const us = localStorage.getItem('username');
+		alert('Logged is as ' + us);
+	})
+}
